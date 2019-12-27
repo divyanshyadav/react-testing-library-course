c03871a7db5882faf48f1d0c551f315193ed9eb6
Divyansh <divyansh.yadav@gmail.com>
2019-12-27 11:11:26 +0530

    add more testcases

--- src/__tests__/http-jest-mock.js
+++ src/__tests__/http-jest-mock.js
@@ -1,21 +1,21 @@
 import React from 'react'
-import {render, fireEvent, wait} from '@testing-library/react'
-import {loadGreeting as mockLoadGreeting} from '../api'
-import {GreetingLoader} from '../greeting-loader-01-mocking'
+import { render, fireEvent, wait } from '@testing-library/react'
+import { GreetingLoader } from '../greeting-loader-01-mocking'
+import { loadGreeting as mockLoadGreeting } from '../api'
 
 jest.mock('../api')
 
-test('loads greetings on click', async () => {
-  const testGreeting = 'TEST_GREETING'
-  mockLoadGreeting.mockResolvedValueOnce({data: {greeting: testGreeting}})
-  const {getByLabelText, getByText} = render(<GreetingLoader />)
+
+test('loads greeting on click', async () => {
+  mockLoadGreeting.mockResolvedValueOnce({ data: { greeting: 'TEST_GREETING' }})
+  const { getByLabelText, getByText } = render(<GreetingLoader />)
   const nameInput = getByLabelText(/name/i)
-  const loadButton = getByText(/load/i)
-  nameInput.value = 'Mary'
+  const loadButton = getByText(/load greeting/i)
+  nameInput.value = 'mary'
+
   fireEvent.click(loadButton)
-  expect(mockLoadGreeting).toHaveBeenCalledWith('Mary')
+  expect(mockLoadGreeting).toBeCalledWith('mary')
   expect(mockLoadGreeting).toHaveBeenCalledTimes(1)
-  await wait(() =>
-    expect(getByLabelText(/greeting/i)).toHaveTextContent(testGreeting),
-  )
+  await wait(() => 
+    expect(getByLabelText(/greeting/i)).toHaveTextContent('TEST_GREETING'))
 })
