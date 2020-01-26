d6f20769dfaeb3d86fb8eee3e51c2547e6186489
Divyansh <divyansh.yadav@gmail.com>
2020-01-26 13:41:48 +0530

    add user-event lib

--- src/__tests__/app-01.js
+++ src/__tests__/app-01.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import {render, fireEvent} from '@testing-library/react'
+import {render} from '@testing-library/react'
+import user from '@testing-library/user-event'
 import {submitForm as mockSubmitForm} from '../api'
 import App from '../app'
 
@@ -8,25 +9,25 @@ jest.mock('../api')
 test('can fill form across multiple pages', async () => {
   mockSubmitForm.mockResolvedValueOnce({ success: true })
 
-  const { getByLabelText, getByText, findByText } = render(<App />)
+  const { findByLabelText, findByText } = render(<App />)
   const testData = {
     food: 'test food',
     drink: 'test drink'
   }
 
-  fireEvent.click(getByText(/fill.*form/i))
-  fireEvent.change(getByLabelText(/food/i), { target: { value: testData.food } })
-  fireEvent.click(getByText(/next/i))
-  fireEvent.change(getByLabelText(/drink/i), { target: { value: testData.drink } })
-  fireEvent.click(getByText(/review/i))
+  user.click(await findByText(/fill.*form/i))
+  user.type(await findByLabelText(/food/i), testData.food)
+  user.click(await findByText(/next/i))
+  user.type(await findByLabelText(/drink/i), testData.drink)
+  user.click(await findByText(/review/i))
 
-  expect(getByLabelText(/food/i)).toHaveTextContent(testData.food)
-  expect(getByLabelText(/drink/i)).toHaveTextContent(testData.drink)
+  expect(await findByLabelText(/food/i)).toHaveTextContent(testData.food)
+  expect(await findByLabelText(/drink/i)).toHaveTextContent(testData.drink)
 
-  fireEvent.click(getByText(/confirm/i, { selector: 'button' }))
-  fireEvent.click(await findByText(/home/i))
+  user.click(await findByText(/confirm/i, { selector: 'button' }))
+  user.click(await await findByText(/home/i))
   expect(mockSubmitForm).toHaveBeenCalledWith(testData)
   expect(mockSubmitForm).toHaveBeenCalledTimes(1)
 
-  expect(getByText(/welcome/i)).toBeInTheDocument()
+  expect(await findByText(/welcome/i)).toBeInTheDocument()
 })
